e170fe69344cc06740a32a41c0f28e5147d0f4cf
xuemaotang <xuemao.tang@fenrir-inc.com.cn>
2016-12-15 10:21:32 +0800

    Add "Do" like Haskell

--- avocado-functional/src/test/java/indi/yume/tools/avocado/functional/FunctionalWhenTest.java
+++ avocado-functional/src/test/java/indi/yume/tools/avocado/functional/FunctionalWhenTest.java
@@ -1,21 +1,29 @@
 package indi.yume.tools.avocado.functional;
 
 import com.annimon.stream.Collectors;
+import com.annimon.stream.Optional;
 import com.annimon.stream.Stream;
 
 import org.junit.Test;
 import org.pcollections.ConsPStack;
 import org.pcollections.PStack;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
+import indi.yume.tools.avocado.functional.doexp.DoOption;
+import indi.yume.tools.avocado.functional.doexp.DoSteamUtil;
+import indi.yume.tools.avocado.functional.doexp.DoUtil;
 import indi.yume.tools.avocado.functional.matching.CaseUtil;
 import indi.yume.tools.avocado.model.tuple.Tuple2;
 import indi.yume.tools.avocado.model.tuple.Tuple3;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.val;
+import rx.Observable;
 import rx.functions.Func1;
+import rx.functions.Func2;
 
 import static indi.yume.tools.avocado.functional.Func.st;
 import static indi.yume.tools.avocado.functional.Utils.const1;
@@ -70,13 +78,13 @@ public class FunctionalWhenTest {
 
     @Test
     public void testGenerateCode_voidFun() {
-        String code = Stream.range(1, 7)
+        String code = com.annimon.stream.Stream.range(1, 7)
                 .map(i ->
                         Tuple3.<String, String, Integer>of(
-                                Stream.range(0, i)
+                                com.annimon.stream.Stream.range(0, i)
                                         .map(n -> String.valueOf(n + 1))
                                         .collect(Collectors.joining(", P", "P", "")),
-                                Stream.range(0, i)
+                                com.annimon.stream.Stream.range(0, i)
                                         .map(n -> String.valueOf(n + 1))
                                         .collect(Collectors.joining(", p", "p", "")),
                                 i))
@@ -92,6 +100,8 @@ public class FunctionalWhenTest {
                                 tuple.getData3()))
                 .collect(Collectors.joining("\n"));
 
+        Func1<Integer, Func1<Integer, String>> curry = Currying.curried((i1, i2) -> i1 + "+" + i2);
+        Func2<Integer, Integer, String> partial = Partial.part2((i1, s2, i3) -> i1 + "+" + s2 + "+" + i3, "2");
         System.out.println(code);
     }
 
@@ -185,5 +195,29 @@ public class FunctionalWhenTest {
                 .empty(() -> "[]")
                 .el_get(l -> l.toString());
         System.out.println(s); // [1, 2, 3]
+    }
+
+    public static <E> PStack<E> reverse(PStack<E> list) {
+        return match(list)
+                .empty(() -> (PStack<E>) Collections.emptyList())
+                .matchF((x, xs) -> reverse(xs.plus(x)))
+                .el_get(list);
+    }
+
+    @Test
+    public void testDoUtil() {
+        DoUtil
+                .do_(() -> Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9))
+                .do_(() -> Observable.just(2, 3))
+                .do_12((t1, t2) -> Observable.just("" + t1 + t2))
+                .yield()
+                .subscribe(System.out::println);
+
+        DoOption
+                .do_(() -> Optional.ofNullable("1"))
+                .do_(() -> Optional.ofNullable("2"))
+                .do_12((x, y) -> Optional.ofNullable(x + y))
+                .return_123((t1, t2, t3) -> t1 + t2 + t3)
+                .ifPresent(System.out::println);
     }
 }
